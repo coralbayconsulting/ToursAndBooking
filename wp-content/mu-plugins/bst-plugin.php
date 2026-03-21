daf6683ba5bed59f426067718499865586273486
Wayne <wayne@coralbayconsulting.com>
2026-03-21 11:12:32 -0400

    Track MU loader bst-plugin.php

---
+++ wp-content/mu-plugins/bst-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * BST Plugins
+ *
+ * @package       BST-plugin
+ * @author        Wayne Wilson
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Blue Strada Tours (BST) Plugin
+ * Plugin URI:        https://www.coralbayconsulting.com.com
+ * Description:       A plugin to manage tours and the booking thereof.
+ * Version:           1.0
+ * Author:            Wayne Wilson, Coral Bay Consulting
+ * Author URI:        https://www.coralbayconsulting.com
+ */
+
+// Include the main plugin file from the subdirectory
+
+require_once __DIR__ . '/bst_plugin/bst-plugin.php';
